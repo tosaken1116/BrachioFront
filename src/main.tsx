316c829215d7082efdaf3e7e4aa3559a93a482ab
tosaken1116 <nagato20021122@gmail.com>
2025-03-18 21:55:52 +0900

    Refactor main.tsx for improved code formatting and update redirect_uri in cognitoAuthConfig

--- src/main.tsx
+++ src/main.tsx
@@ -14,33 +14,33 @@ const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
+  interface Register {
+    router: typeof router;
+  }
 }
 
 async function enableMocking() {
-	if (!import.meta.env.DEV) return;
-	const { worker } = await import("./mocks/browser");
-	return worker.start();
+  if (!import.meta.env.DEV) return;
+  const { worker } = await import("./mocks/browser");
+  return worker.start();
 }
 const cognitoAuthConfig = {
-	authority: `${import.meta.env.VITE_COGNITO_AUTHORITY_URL}`,
-	client_id: `${import.meta.env.VITE_COGNITO_CLIENT_ID}`,
-	redirect_uri: "http://localhost:5173",
-	response_type: "code",
-	scope: "email openid phone",
+  authority: `${import.meta.env.VITE_COGNITO_AUTHORITY_URL}`,
+  client_id: `${import.meta.env.VITE_COGNITO_CLIENT_ID}`,
+  redirect_uri: `${import.meta.env.VITE_REDIRECT_URI}`,
+  response_type: "code",
+  scope: "email openid phone",
 };
 // Render the app
 enableMocking().then(() => {
-	// biome-ignore lint/style/noNonNullAssertion: <explanation>
-	const rootElement = document.getElementById("root")!;
-	if (!rootElement.innerHTML) {
-		const root = ReactDOM.createRoot(rootElement);
-		root.render(
-			<AuthProvider {...cognitoAuthConfig}>
-				<RouterProvider router={router} />
-			</AuthProvider>,
-		);
-	}
+  // biome-ignore lint/style/noNonNullAssertion: <explanation>
+  const rootElement = document.getElementById("root")!;
+  if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <AuthProvider {...cognitoAuthConfig}>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    );
+  }
 });
